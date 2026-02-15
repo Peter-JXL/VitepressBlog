1ecc3c20a4f209617e120eaf7558e3f6dd6139ce
peterjxl <peterjxl@qq.com>
2026-02-15 17:32:54 +0800

    ➖ 去除 RSS 插件，mermaid 插件以及相关配置，ora 插件

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,13 +4,7 @@ import type { Theme } from 'vitepress';
 import "vitepress-theme-teek/index.css";
 import "./style/index.scss";  // 引入 .vitepress/theme/style/index.scss 全局样式
 import "vitepress-theme-Teek/vp-plus/nav-blur.scss"; // 导航栏毛玻璃
-import { initComponent } from 'vitepress-plugin-legend/component';
-import 'vitepress-plugin-legend/dist/index.css';
-
 
 export default {
-  extends: Teek,
-  enhanceApp({app}) {
-    initComponent(app);  // mermaid 支持
-  },
-} satisfies Theme;
+  extends: Teek
+}
