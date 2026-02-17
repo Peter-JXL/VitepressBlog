5e93c144309f386867d8ecf08c72de7dbb58f4fb
peterjxl <peterjxl@qq.com>
2026-02-17 11:24:15 +0800

    💄 添加样式文件

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,11 +1,22 @@
 // .vitepress/theme/index.ts
 import Teek from "vitepress-theme-teek";
 import type { Theme } from 'vitepress';
+
+// 引入样式
+import "./style/index.scss";  // 自定义的全局样式
 import "vitepress-theme-teek/index.css";
-import "./style/index.scss";  // 全局样式
-import "vitepress-theme-Teek/vp-plus/nav-blur.scss"; // 导航栏毛玻璃
+import "vitepress-theme-Teek/vp-plus/nav-blur.scss";          // 导航栏毛玻璃
+import "vitepress-theme-teek/theme-chalk/tk-mark.css";        // <mark></mark> 样式
+import "vitepress-theme-teek/theme-chalk/tk-blockquote.css";  // > 引用块样式
+import "vitepress-theme-teek/theme-chalk/tk-aside.css";       // 右侧目录栏文字悬停和激活样式
+import "vitepress-theme-teek/theme-chalk/tk-doc-fade-in.css"; // 文章页淡入效果
+import "vitepress-theme-teek/theme-chalk/tk-nav-translation.css"; // 导航栏深色、浅色模式切换按钮样式
+ 
+
+// 自定义组件，例如 404，贡献日历
 import TeekLayoutProvider from "./components/TeekLayoutProvider.vue";
 
+// mermaid 插件
 import { initComponent } from 'vitepress-plugin-legend/component';
 import 'vitepress-mermaid-preview/dist/index.css';
 
