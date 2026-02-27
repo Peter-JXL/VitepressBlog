c769edefc6f70526c14edba85e00e06057a4794c
peterjxl <peterjxl@qq.com>
2026-02-27 14:24:37 +0800

    🔧拆分出背景图片配置

--- docs/.vitepress/teekConfig.ts
+++ docs/.vitepress/teekConfig.ts
@@ -1,6 +1,7 @@
 // 主题配置文件，会被 ./config.mts 引用
 import { defineTeekConfig } from "vitepress-theme-teek/config";
 import { SocialLinks } from "./config/SocialLinks"; //导入社交链接配置
+import { BodyBgImgs } from "./config/bodyBgImg"; //导入社交链接配置
 import { vitepressPluginLegend } from 'vitepress-plugin-legend';
 
 
@@ -100,6 +101,7 @@ export const teekConfig = defineTeekConfig({
   },
 
   social: SocialLinks,   // 社交图标，显示于博主信息栏和页脚栏。参考 https://vp.teek.top/guide/icon-use.html#社交图标-iconfont
+  bodyBgImg: BodyBgImgs,   // 背景图片配置，将整个网站的背景色改为图片。
 
   // 精选文章卡片配置
   topArticle: {
@@ -107,162 +109,6 @@ export const teekConfig = defineTeekConfig({
     limit: 5, // 一页显示的数量
     autoPage: true, // 是否自动翻页
     dateFormat: "yyyy-MM-dd hh:mm:ss", // 精选文章的日期格式
-  },
-
-  // 背景图片配置，将整个网站的背景色改为图片。
-  bodyBgImg: {
-    imgSrc: [
-      'https://image.peterjxl.com/blog/2026-02-23.jpg',   //  绝区零 妄想天使
-      'https://image.peterjxl.com/blog/140830123_p0.jpg', // 绝区零 妄想天使
-      'https://image.peterjxl.com/blog/140018269_p0.jpg', // 星穹铁道 群星
-      'https://image.peterjxl.com/blog/134420820_p0.jpg', // 流萤
-      
-      'https://image.peterjxl.com/blog/140831812_p0.jpg', // 鸣潮 爱弥斯
-      'https://image.peterjxl.com/blog/140161372_p0.jpg', // 原神群星 哥伦比娅
-      'https://image.peterjxl.com/blog/141274290_p0.jpg', // 哥伦比娅 + 桑多涅
-      'https://image.peterjxl.com/blog/141050669_p0.jpg', // 哥伦比娅
-      'https://image.peterjxl.com/blog/140002549_p1.jpg', // 哥伦比娅
-      'https://image.peterjxl.com/blog/139811173_p0.jpg', // 哥伦比娅
-      'https://image.peterjxl.com/blog/138613692_p0.jpg', // 哥伦比娅
-      'https://image.peterjxl.com/blog/140014451_p0.jpg', // 哥伦比娅
-      'https://image.peterjxl.com/blog/139980512_p0.jpg', // 哥伦比娅 雅珂达 派蒙
-
-
-      'https://image.peterjxl.com/blog/139754531_p0.jpg', //  琳奈
-      'https://image.peterjxl.com/blog/2025-12-25-2.jpg',   // 琳奈
-      'https://image.peterjxl.com/blog/2025-12-25-1.jpg',   // 琳奈
-
-      'https://image.peterjxl.com/blog/139955382_p0.jpg', // 奥古斯塔
-      'https://image.peterjxl.com/blog/139955461_p0.jpg', // 奥古斯塔
-      'https://image.peterjxl.com/blog/2025-3-23.jpg',  // 坎特蕾拉
-
-
-      // 原神
-      'https://image.peterjxl.com/blog/134.jpg',  // 胡桃
-      'https://image.peterjxl.com/blog/125077204_p0.jpg', // 胡桃
-      'https://image.peterjxl.com/blog/122658264_p0.jpg', // 胡桃&芙芙 
-      'https://image.peterjxl.com/blog/138439477_p0.jpg', // 芙芙 
-      'https://image.peterjxl.com/blog/102890180_p1.png', // 芙芙
-      'https://image.peterjxl.com/blog/136656625_p0.jpg', // 芙芙
-
-      'https://image.peterjxl.com/blog/138422625_p0.jpg', // 奈芙尔 原神 雅珂达 
-      'https://image.peterjxl.com/blog/104486407_p0.jpg', // 雷电将军
-      'https://image.peterjxl.com/blog/131754182_p0.jpg', // 丝柯克
-      'https://image.peterjxl.com/blog/2026-02-04.jpg',   // 原神 兹白
-
-      // 崩坏 3
-      'https://image.peterjxl.com/blog/Elysia.jpg', // 爱莉
-      'https://image.peterjxl.com/blog/100649660_p1.png', // 爱莉
-      'https://image.peterjxl.com/blog/124205470_p0.jpg', // 爱莉
-      'https://image.peterjxl.com/blog/101693777_p0.jpg', // 爱莉
-      'https://image.peterjxl.com/blog/134156383_p0.jpg', // 爱莉
-      'https://image.peterjxl.com/blog/140659332_p0.jpg', // 爱莉希雅
-      'https://image.peterjxl.com/blog/137445639_p0.jpg', // 爱丽希雅 崩铁
-      'https://image.peterjxl.com/blog/2024-6-2-9.png', // 松雀
-      'https://image.peterjxl.com/blog/2024-11-10-12.png', // 松雀
-      'https://image.peterjxl.com/blog/Av1352530965.jpg', // 瑟莉姆
-      'https://image.peterjxl.com/blog/2024-4-8-2.png', // 松雀和瑟莉姆
-      'https://image.peterjxl.com/blog/2024-10-12.png', // 薇塔
-      'https://image.peterjxl.com/blog/2024-09-26-3.png', // 薇塔和花火
-      'https://image.peterjxl.com/blog/2024-11-10-4.png', // 薇塔和花火
-      'https://image.peterjxl.com/blog/2024-11-10-6.png', // 薇塔和花火
-      'https://image.peterjxl.com/blog/2024-10-12-4.png', // 格蕾修
-
-      // 绝区零
-      'https://image.peterjxl.com/blog/%E6%8E%A8%E7%89%B9%20@rina_0A%202024-7-6.jfif', // 妮可躺在推车上
-      'https://image.peterjxl.com/blog/122710477_p0.jpg',  // 妮可+鲨鱼妹
-      'https://image.peterjxl.com/blog/121269323_p0.jpg',  // 鲨鱼妹
-      'https://image.peterjxl.com/blog/123353563_p0.png',  // 柏妮思
-
-      // 鸣潮
-      'https://image.peterjxl.com/blog/2025-6-12.jpg',    // 卡提希娅 飘然远去的长梦-画师：撸卜
-      'https://image.peterjxl.com/blog/131473201_p0.jpg', // 卡提希娅
-      'https://image.peterjxl.com/blog/2025-08-29.jpg',  // 今汐
-      'https://image.peterjxl.com/blog/121206620_p1.png', // 长离
-      'https://image.peterjxl.com/blog/120767239_p0.png', // 长离
-      'https://image.peterjxl.com/blog/119879001_p0.png', // 吟霖
-      'https://image.peterjxl.com/blog/125117828_p0.jpg', // 吟霖
-      'https://image.peterjxl.com/blog/125670168_p0.jpg', // 吟霖
-      'https://image.peterjxl.com/blog/2025-04-07.jpg',   // 坎特蕾拉
-      'https://image.peterjxl.com/blog/134417892_p0.jpg', // 奥古斯塔
-      'https://image.peterjxl.com/blog/134440043_p0.jpg', // 奥古斯塔
-      'https://image.peterjxl.com/blog/134452846_p0.jpg', // 奥古斯塔
-      'https://image.peterjxl.com/blog/2025-09-02.jpg',    // 奥古斯塔
-      'https://image.peterjxl.com/blog/137499724_p0.jpg', // 赞妮
-
-      // 星穹铁道    
-      'https://image.peterjxl.com/blog/2025-05-04.jpg',  // 遐蝶
-      'https://image.peterjxl.com/blog/2025-02-07.jpg', // 遐蝶
-      'https://image.peterjxl.com/blog/128181590_p0.jpg', // 遐蝶
-      'https://image.peterjxl.com/blog/130724632_p2.jpg', // 风堇
-      'https://image.peterjxl.com/blog/121642710_p1.png', // 云璃
-      'https://image.peterjxl.com/blog/2025-12-31.jpg',   // 流萤
-      'https://image.peterjxl.com/blog/119791514_p0.jpg', // 流萤
-      'https://image.peterjxl.com/blog/116629910_p0.jpg', // 流萤
-      'https://image.peterjxl.com/blog/2025-09-05.jpg',  // 海瑟音
-      'https://image.peterjxl.com/blog/121154474_p11.jpg',// Kafka
-      'https://image.peterjxl.com/blog/111072628_p0.png', // Kafka
-      'https://image.peterjxl.com/blog/109349799_p0.jpg', // Kafka      
-      'https://image.peterjxl.com/blog/116526272_p0.jpg', // 花火
-      'https://image.peterjxl.com/blog/117466601_p0.png', // 黄泉、花火
-      'https://image.peterjxl.com/blog/139436858_p0.jpg',   // 花火
-      'https://image.peterjxl.com/blog/124529422_p0.png', // 三月七，星
-      'https://image.peterjxl.com/blog/2025-10-17.jpg', // 三月七
-      'https://image.peterjxl.com/blog/2025-09-30.jpg', // 长夜月
-      'https://image.peterjxl.com/blog/2024-12-26-4.jpg', // 知更鸟
-      'https://image.peterjxl.com/blog/125643183_p0.png', // 停云
-      'https://image.peterjxl.com/blog/126508781_p0.jpg', // 大黑塔
-      'https://image.peterjxl.com/blog/2025-02-07-6.jpg', // 大黑塔
-      'https://image.peterjxl.com/blog/126424846_p0.jpg', // 大黑塔
-      'https://image.peterjxl.com/blog/129916649_p0.jpg', // 缇宝
-      'https://image.peterjxl.com/blog/131998699_p0.jpg', // 缇宝
-
-      // 日常
-      'https://image.peterjxl.com/backgruond-city.jpg',
-
-      // re: zero
-      'https://image.peterjxl.com/blog/re-background.jpg',
-      'https://image.peterjxl.com/blog/Emilia.jpg',
-      'https://image.peterjxl.com/blog/Emilia-2.jpg',
-
-      // Miku
-      'https://image.peterjxl.com/blog/120586024_p0.png',
-      'https://image.peterjxl.com/blog/114179851_p0.jpg',
-      'https://image.peterjxl.com/blog/120814179_p0.jpg',
-
-      // 孤独摇滚
-      'https://image.peterjxl.com/blog/112280380_p0.png',
-      'https://image.peterjxl.com/blog/103907445_p0.png',
-      'https://image.peterjxl.com/blog/109565821_p0.png',
-
-      // 二次元
-      'https://image.peterjxl.com/blog/180.jpg',  // 下拉裙摆
-      'https://image.peterjxl.com/blog/4.2b.png', // 尼尔·机械纪元
-      'https://image.peterjxl.com/blog/116406855_p0.png', // 莉可莉丝
-      'https://image.peterjxl.com/blog/2020-04-03_16-23-36.png', // 鬼灭之刃
-      'https://image.peterjxl.com/blog/114613535_p0.png', // 葬送的芙莉莲
-      'https://image.peterjxl.com/blog/76601747_p1.jpg', // 国家队
-      'https://image.peterjxl.com/blog/62494753_p0.jpg', // 埃罗芒
-      'https://image.peterjxl.com/blog/Mikasa.jpg',  // 三笠
-      'https://image.peterjxl.com/blog/saber.jpg',  // fate
-      'https://image.peterjxl.com/blog/123800363_p0.jpg', // 夏日重现
-      'https://image.peterjxl.com/blog/gojo-eyes-jujutsu-kaisen-hd-wallpaper-uhdpaper.com-208@2@a.jpg', // 五条悟
-      'https://image.peterjxl.com/blog/135718569_p0.jpg', // 蕾塞
-
-      // 冰菓
-      'https://image.peterjxl.com/blog/690112.jpg',
-      'https://image.peterjxl.com/blog/bingguo.jpg',
-
-      'https://image.peterjxl.com/blog/walk-in-space.png', // 太空
-      'https://image.peterjxl.com/blog/182.jpg', // 洛基
-      'https://image.peterjxl.com/blog/miaomiao.jpg', // 妙妙！
-      'https://image.peterjxl.com/blog/2025-08-28.jpg', // 烤鱼生日
-      'https://image.peterjxl.com/blog/MEME-Kaofish.jpg', // 烤鱼
-    ],
-    imgOpacity: 1, // 背景图透明度，选值 0.1 ~ 1.0
-    imgInterval: 30000, //  当有多张背景图时（imgSrc 为数组），设置切换时间，单位：毫秒
-    imgShuffle: false, // 是否随机切换
-    mask: false // 背景图遮罩
   },
 
   // 友情链接卡片配置
