b25f11463c4323474733f50456fb0ec8f5b98f05
peterjxl <peterjxl@qq.com>
2026-06-20 17:37:51 +0800

    💄 添加背景图

--- docs/.vitepress/config/BackgroundImg.ts
+++ docs/.vitepress/config/BackgroundImg.ts
@@ -1,6 +1,9 @@
 // 博客背景图地址列表，也是默认封面图地址列表，导出给 teekConfig.ts 使用
 
 export const Imgs: Array<string> = [
+  'https://image.peterjxl.com/blog/X@nmgmkn_20260619.jpg',  // 绝区零 柏妮思
+  'https://image.peterjxl.com/blog/145973695_p0.jpg', // 明日方舟 洛茜
+  'https://image.peterjxl.com/blog/146059935_p0.jpg', // 瑕蝶
   'https://image.peterjxl.com/blog/129487999_p0.jpg', // 菲比
   'https://image.peterjxl.com/blog/133464975_p0.jpg', // 大昔涟
   'https://image.peterjxl.com/blog/X@226083260Bubai-20260513.jpg', // 荧妹
